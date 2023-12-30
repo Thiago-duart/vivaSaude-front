4dbd5710bc4ad01040c536002b357e15efb44947
ThiagoDuart <td1405150+github@gmail.com>
2023-12-30 20:38:40 -0300

    feat: create new exports

--- app/interface/index.tsx
+++ app/interface/index.tsx
@@ -1,5 +1,7 @@
 //components
 export { Header } from "./components/Header/Header";
-
+export { Button } from "./components/Button/button";
+export { Input } from "./components/Input/input";
+export { PasswordInput } from "./components/PasswordInput/PasswordInput";
 //hooks
 export * from "./components/hooks/useTheme";

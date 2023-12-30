1ff78e301448144a4baf89e42a21d15db56ce8cc
ThiagoDuart <td1405150+github@gmail.com>
2023-12-30 05:28:39 -0300

    feat: Create Header component

---
+++ app/interface/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { ChefHat, Moon, Sun } from "lucide-react";
+import { useContext } from "react";
+import { ThemeContext } from "../..";
+
+export function Header() {
+  const { useTheme, theme, setTheme } = useContext(ThemeContext);
+  return (
+    <>
+      <header className="flex items-center  h-16 px-8 bg-grayBackground text-white ">
+        <nav className="flex  justify-between w-full cursor-pointer">
+          <div className="flex gap-6 ">
+            <div className="flex items-center gap-2">
+              <ChefHat color="white" size={26} />
+              <p className="font-bold">VivaSaude</p>
+            </div>
+            <div>
+              <p>Recursos</p>
+            </div>
+            <div>
+              <p>Soluções</p>
+            </div>
+          </div>
+          <div className="flex gap-6 items-center">
+            <div>
+              {theme == "dark" ? (
+                <Sun
+                  size={18}
+                  onClick={() => {
+                    useTheme(), setTheme("light");
+                  }}
+                />
+              ) : (
+                <Moon
+                  color="white"
+                  size={18}
+                  onClick={() => {
+                    useTheme(), setTheme("dark");
+                  }}
+                />
+              )}
+            </div>
+            <div>
+              <p>Login</p>
+            </div>
+            <div>Cadastrar</div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}

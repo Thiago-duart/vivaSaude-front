94d0d16d76a046cc4c7d0bab3e57d0f93b5fc936
ThiagoDuart <td1405150+github@gmail.com>
2024-01-03 05:17:13 -0300

    feat: create actionSingUp

---
+++ app/singup/action.ts
@@ -0,0 +1,13 @@
+import { useParse } from "../interface/components/hooks/useParse";
+import { singUpSchema } from "./schema/singUp-schema";
+
+export async function actionSingUp(previousState: any, formData: FormData) {
+  const data = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  const responseParse = useParse(singUpSchema, data);
+
+  return responseParse;
+}

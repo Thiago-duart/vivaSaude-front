219b477741a764e773eddaa04a166ec2114f3440
ThiagoDuart <td1405150+github@gmail.com>
2024-01-03 04:52:57 -0300

    feat: create actionLogin

---
+++ app/login/action.ts
@@ -0,0 +1,12 @@
+import { useParse } from "../interface/components/hooks/useParse";
+import { loginSchema } from "./schema/login-schema";
+
+export async function actionLogin(previousState: any, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  const responseParse = useParse(loginSchema, data);
+
+  return responseParse;
+}

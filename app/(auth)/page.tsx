1f70b91184ac47624aa596125b740050596aaa55
ThiagoDuart <td1405150+github@gmail.com>
2024-01-09 15:04:16 -0300

    feat: create temporary page

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,7 +1,19 @@
 export default function InitialPage() {
   return (
     <>
-      <p>teste</p>
+      <div className="w-full h-[90vh] 2xl:h-[93.3vh] dark:bg-grayBackground">
+        <div className="flex flex-col gap-5 items-center  py-32">
+          <h1 className="dark:text-white text-[3rem]">
+            Viva uma vida saudável
+          </h1>
+          <p className="dark:text-white w-1/2">
+            {" "}
+            Descubra o poder de organizar suas receitas e gerenciar suas
+            calorias de forma simples e eficiente. Experimente nosso site e
+            encontre o equilíbrio perfeito entre sabor e saúde!
+          </p>
+        </div>
+      </div>
     </>
   );
 }

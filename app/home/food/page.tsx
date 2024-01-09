37a7cfcfb9b5af1e087c468658380455ed08e5e4
ThiagoDuart <td1405150+github@gmail.com>
2024-01-09 15:01:45 -0300

    feat: create page food

---
+++ app/home/food/page.tsx
@@ -0,0 +1,14 @@
+import { ChangeWeek } from "@/app/interface";
+
+export default function Food() {
+  return (
+    <>
+      <div>
+        <ChangeWeek name="Alimentação" />
+      </div>
+      <div className="flex flex-wrap justify-center gap-6 p-12 w-full">
+        {/* <Card /> */}
+      </div>
+    </>
+  );
+}
